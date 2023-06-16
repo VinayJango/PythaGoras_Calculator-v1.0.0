5407e9891fa718a6c5502d2252b371ba0bd67efe
Vinay jangam <84473308+VinayJango@users.noreply.github.com>
2023-06-16 23:27:15 +0530

    Add files via upload

---
+++ Pythagoras Theorem Calculator/PythaGoras_Theorem.h
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+void App_Banner()
+{
+	cout << "Code Owned By- VinayJango | https://github.com/VinayJango"<<endl;
+}
+/* Pythagoras Theorem is (ONE SIDE SQR) + (OTHER SIDE SQR) = (HYPO)*/
+float v1, v2, Hypo;
+void Calculate_Hypo()
+{
+	while ((v1 > 0) && (v2 > 0)); // While The Given Value 1 and 2 is Greater Than Zero
+	{
+		cout << "Enter The Value Of The First Side: ";
+		cin >> v1;
+		cout << "Enter The Value Of The Second Side: ";
+		cin >> v2;
+		Hypo = sqrt((v1 * v1) + (v2 * v2));
+		cout << "The Hypotenuse Length is: " << (Hypo);
+		cout << "Bye~!" << endl;
+	}
+	
+}
+
+
+
---
+++ Pythagoras Theorem Calculator/Pythagoras Theorem Calculator.cpp
@@ -0,0 +1,9 @@
+#include "PythaGoras_Theorem.h"
+#include <iostream>
+#include <cmath>
+int main()
+{
+	App_Banner();
+	Calculate_Hypo();
+}
+//Author - VinayJango | Follow At - https://github.com/VinayJango
